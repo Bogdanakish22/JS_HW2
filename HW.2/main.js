8faf75a2e43b58ea4b1fa19053f85bcb448d9894
Kish Bogdana <kishbogdana22@gmail.com>
2025-01-07 17:21:17 +0100

    add

---
+++ HW.2/main.js
@@ -0,0 +1,61 @@
+let temperatureCelsius = 30;
+let temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+console.log(`${temperatureCelsius}°C = ${temperatureFahrenheit}°F`);
+
+// -------------------------------------------------------------
+
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = hoursInMonth * 60;
+console.log(daysInMonth);
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+// -------------------------------------------------------------
+
+let health = 100;
+let energy = 50;
+
+health -= 20;
+energy -= 10;
+
+console.log(health);
+console.log(energy);
+
+
+// -------------------------------------------------------------
+
+let total = 400;
+let discount = total * 0.9;
+console.log(discount);
+
+
+// -------------------------------------------------------------
+
+const floatingNumber = 5.43;
+console.log(Math.floor(floatingNumber));
+
+// -------------------------------------------------------------
+const stringNumber = "37.15"
+console.log(parseFloat(stringNumber)) 
+
+// -------------------------------------------------------------
+
+const stringNumberInt = "38"
+console.log(parseInt(stringNumberInt))
+
+// -------------------------------------------------------------
+
+const numderMath = 57;
+console.log(Math.sqrt(numderMath));
+
+
+// -------------------------------------------------------------
+
+const number = 39;
+const numberString = "12";
+
+console.log(parseInt(numberString))
+console.log(number.toString())
+
+// -------------------------------------------------------------
